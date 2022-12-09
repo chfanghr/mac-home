59e2068efecfded76539d2040b7eb11aeebb3195
Hongrui Fang <chfanghr@gmail.com>
2022-12-09 18:09:13 +0800

    try helix

--- flake.nix
+++ flake.nix
@@ -46,25 +46,25 @@
     username = "fanghr";
     findModules = dir:
       builtins.concatLists (builtins.attrValues (builtins.mapAttrs
-      (name: type:
-        if type == "regular"
-        then [
-          {
-            name = builtins.elemAt (builtins.match "(.*)\\.nix" name) 0;
-            value = dir + "/${name}";
-          }
-        ]
-        else if
-          (builtins.readDir (dir + "/${name}"))
-          ? "default.nix"
-        then [
-          {
-            inherit name;
-            value = dir + "/${name}";
-          }
-        ]
-        else findModules (dir + "/${name}"))
-      (builtins.readDir dir)));
+        (name: type:
+          if type == "regular"
+          then [
+            {
+              name = builtins.elemAt (builtins.match "(.*)\\.nix" name) 0;
+              value = dir + "/${name}";
+            }
+          ]
+          else if
+            (builtins.readDir (dir + "/${name}"))
+            ? "default.nix"
+          then [
+            {
+              inherit name;
+              value = dir + "/${name}";
+            }
+          ]
+          else findModules (dir + "/${name}"))
+        (builtins.readDir dir)));
     profiles = builtins.listToAttrs (findModules ./profiles);
   in rec {
     checks.${system} = {
@@ -95,6 +95,7 @@
           zsh
           man
           git
+          helix
         ];
       };
     };
