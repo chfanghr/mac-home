4344211e5d38a59bb69aa258cd21edf6127b1429
Hongrui Fang <chfanghr@gmail.com>
2023-11-14 07:45:33 +0800

    suppress warning

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -61,6 +61,8 @@
       "flush_routes" = "sudo route -n flush";
     };
     initExtraFirst = ''
+      typeset -g POWERLEVEL9K_INSTANT_PROMPT=quiet
+
       if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
